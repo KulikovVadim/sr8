2928a0181088c2d6d714dd6c5377efa467203a4a
KulikovVadim <pritinovin@gmail.com>
2021-10-08 20:31:00 +0300

    sr8

---
+++ 8.py
@@ -0,0 +1,23 @@
+def perevod_chisla(chislo, sistema):
+    ost = ""
+    while chislo > 0:
+        ost = str(ost) + str(chislo % sistema)
+        chislo = int(chislo) // sistema
+    return ost[::-1]
+
+try:
+    chislo = int(input("Введите целое число, которое вы бы хотели перевести: "))
+    sistema = int(input("Выберите систему счисления, в которую вы бы хотели перевести число(двоичную либо восьмеричную): "))
+except ValueError:
+    print("Ошибка, пожалуйста, введите целые числовое значение")
+else:
+    if sistema == 2 or sistema == 8:
+        perevod_chisla(chislo, sistema)
+        print(str(chislo) + ' -> ' + str(perevod_chisla(chislo, sistema)))
+    else:
+        print("Пожалуйста, выберите двоичную либо восьмиричную систему")
+
+
+
+
+
